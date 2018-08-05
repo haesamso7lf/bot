9a59d52b0765b43d04c2545a43ee6df800971be1
neopanther <amaiplusplus@gmail.com>
2018-08-05 02:32:53 +0200

    configuring for Heroku

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     request = require('request');
 var app = express();
+app.get('/', (req, res) => {
+    res.send('facebook api');
+});
+
 app.get('/webhook', (req, res) => {
     res.send('hello');
 });
