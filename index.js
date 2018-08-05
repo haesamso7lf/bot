b2d486b8c55f44be21a68a2637d5c0d8dae36fd5
neopanther <amaiplusplus@gmail.com>
2018-08-05 03:14:31 +0200

    adding body parser

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     request = require('request');
 var app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 var PORT = process.env.PORT || 3000
 app.get('/', (req, res) => {
     res.send('facebook api');
@@ -11,6 +15,7 @@ app.get('/webhook', (req, res) => {
     if(req.query["hub.verify_token"] == "myTokenAndShit") {
         req.send(req.query["hub.challenge"]);
     }
+    res.send('wrong token')
 });
 
 app.listen(PORT, () => {
